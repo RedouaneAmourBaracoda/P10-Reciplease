c1d108be529ef8213dde8e3cb9968b3009ba3ba4
RedouaneAmourBaracoda <redouane.amour@baracoda.com>
2024-11-24 00:17:26 +0100

    create starter project.

---
+++ Reciplease/Reciplease/RecipleaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipleaseApp.swift
+//  Reciplease
+//
+//  Created by Redouane on 24/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct RecipleaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
