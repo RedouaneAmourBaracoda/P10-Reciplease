cea14998e27db07c4995418fa33922cade228e01
RedouaneAmourBaracoda <redouane.amour@baracoda.com>
2025-01-15 10:30:07 +0100

    remove todo list.

--- Reciplease/Sources/Views/Launch/RecipleaseApp.swift
+++ Reciplease/Sources/Views/Launch/RecipleaseApp.swift
@@ -7,15 +7,6 @@
 
 import SwiftUI
 
-// MARK: - TODO:
-
-// Pour le green coding: parler du cache pour les images, et qu'on limite les appels réseaux avec le disabled button.
-// Pour la présentation: parler de deux fichiers :
-// 1. le privacy manifest dans xcode pour l'utilisation d'api apple comme USER DEFAULT CORELOCATION etc.
-// 2. le formulaire apple (Apple privacy form developer) a preparer avant soumission avec des questions.
-// Être capable d'expliquer comme preparer la soumission sur l'app store avec : les deux fichiers cités plus haut, le bundle id de l'app, le code signing (un certificat et provisioning profile).
-// Ajouter des accessibilité label pour voicover
-
 @main
 struct RecipleaseApp: App {
     var body: some Scene {
